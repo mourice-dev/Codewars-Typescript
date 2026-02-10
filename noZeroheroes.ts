c32d44fdf6fe3c840a183420bb8b120682fe405a
nshuti <145772623+mourice-dev@users.noreply.github.com>
2026-02-10 18:26:33 +0200

    Add noZeroheroes solution

---
+++ noZeroheroes.ts
@@ -0,0 +1,34 @@
+/** @format */
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// 1450   -> 145
+// 960000 -> 96
+// 1050   -> 105
+// -1050  -> -105
+// 0      -> 0
+// Note: Zero should be left as it is.
+
+// export function noBoringZeros(n: number) {
+//   var nn = n.toString().split("");
+//   const nx: number[] = nn.map(Number);
+//   while (nx.slice(-1).values === "0") {
+//      nn.splice(0, -1);
+//   }
+
+//   return nx;
+// }
+// console.log(noBoringZeros(1450));
+
+export function noBoringZeros(n: number) {
+  if (n === 0) return 0;
+  let nn = n.toString().split("");
+  // .pop() returns the last string element, so the comparison works!
+  while (nn[nn.length - 1] === "0") {
+    nn.pop();
+  }
+  return Number(nn.join(""));
+}
