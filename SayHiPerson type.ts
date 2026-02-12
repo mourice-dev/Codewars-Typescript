34646e34a4bef7112d188de9eb64e61741d69472
nshuti <nshutikope@gmail.com>
2026-02-12 12:00:00 +0200

    Add solutions for Counting Sheep, SayHiPerson and IsItEven

---
+++ SayHiPerson type.ts
@@ -0,0 +1,12 @@
+// Write a function sayHi which will take a Person record as input and will return a string:
+
+// "Hi, i'am {firstName} {lastName} and it is nice to meet You."
+
+// You will need to define the record type Person, with fields firstName and lastName.
+
+// module PersonSaysHi where
+
+// data Person = Person { firstName :: String, lastName :: String }
+
+// sayHi :: Person -> String
+// sayHi p = "Hi"
