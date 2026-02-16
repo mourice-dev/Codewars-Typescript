a0ebb80d5da7f9754e3dc2fcb9ec500c71a03730
nshuti <nshutikope@gmail.com>
2026-02-16 20:18:41 +0200

    Implement role-based authorization with error handling for unknown roles

---
+++ neverType.ts
@@ -0,0 +1,64 @@
+/** @format */
+
+// type Alphanumeric = string & number; // never
+// console.log(Alphanumeric)
+
+// function raiseError(message: string): never {
+//     throw new Error(message);
+// }
+
+// type Role = 'admin' | 'user';
+
+// const authorize = (role: Role): string => {
+//   switch (role) {
+//     case 'admin':
+//       return 'You can do anything';
+//     case 'user':
+//       return 'You can do something';
+//     default:
+//       // never reach here util we add a new role
+//       const _unreachable: never = role;
+//       throw new Error(`Invalid role: ${_unreachable}`);
+//   }
+// };
+
+
+
+// type Role = 'admin' | 'user' | 'guest';
+
+// const authorize = (role: Role): string => {
+//   switch (role) {
+//     case 'admin':
+//       return 'You can do anything';
+//     case 'user':
+//       return 'You can do something';
+//     case 'guest':
+//       return 'You can do nothing';
+//     default:
+//       // never reach here util we add a new role
+//       const _unreachable: never = role;
+//       throw new Error(`Invalid role: ${_unreachable}`);
+//   }
+// };
+type Role = 'admin' | 'user' | 'guest';
+
+const unknownRole = (role: never): never => {
+  throw new Error(`Invalid role: ${role}`);
+};
+
+const authorize = (role: Role): string => {
+  switch (role) {
+    case 'admin':
+      return 'You can do anything';
+    case 'user':
+      return 'You can do something';
+    case 'guest':
+      return 'You can do nothing';
+    default:
+      // never reach here util we add a new role
+      return unknownRole(role);
+  }
+};
+
+console.log(authorize('admin'));
+
