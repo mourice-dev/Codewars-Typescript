de7767b800095db334dab261eea3509b73e0c0cd
nshuti <nshutikope@gmail.com>
2026-02-19 08:38:22 +0200

    fix: Correct Person constructor and getAll method implementation

--- protoType.ts
+++ protoType.ts
@@ -1,16 +1,65 @@
 /** @format */
 
-let Person = (Firstname: string, Lastname : string){
-    this.Firstname = Firstname,
-    this.Lastname = Lastname
+function Person ( this: any , Firstname: any, Lastname: any ) {
+  this.Firstname = Firstname;
+  this.Lastname = Lastname;
+};
 
-}
-Person.prototype.getAll  = function () : string {
-    return `${Person.Firstname} ${Person.Lastname}`
-}
-let person = new Person("nshuti", "maurice");
-console.log(person.getAll())
+Person.prototype.getAll = function () {
+  return `${this.Firstname} ${this.Lastname}`;
+};
 
+let person = new (Person as any) ("nshuti", "maurice");
+console.log(person.getAll());
+
+// function Person(ssn, firstName, lastName) {
+//     this.ssn = ssn;
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+// }
+
+// Person.prototype.getFullName = function () {
+//     return `${this.firstName} ${this.lastName}`;
+// }
+
+// let person = new Person('171-28-0926','John','Doe');
+// console.log(person.getFullName());
+
+// function Person (Firstname, Lastname) {
+//   this.Firstname = Firstname;
+//   this.Lastname = Lastname;
+// };
+
+// Person.prototype.getAll = function () {
+//   return `${this.Firstname} ${this.Lastname}`;
+// };
+
+// let person = new Person("nshuti", "maurice");
+// console.log(person.getAll());
+
+// function Person (Firstname, Lastname) {
+//   this.Firstname = Firstname;
+//   this.Lastname = Lastname;
+// };
+
+// Person.prototype.getAll = function () {
+//   return `${this.Firstname} ${this.Lastname}`;
+// };
+
+// let person = new Person("nshuti", "maurice");
+// console.log(person.getAll());
+
+// function Person(this: any, Firstname: string, Lastname: string) {
+//   this.Firstname = Firstname;
+//   this.Lastname = Lastname;
+// }
+
+// Person.prototype.getAll = function (): string {
+//   return `${this.Firstname} ${this.Lastname}`;
+// };
+
+// let person = new (Person as any)("nshuti", "maurice");
+// console.log(person.getAll());
 
 // function Person(ssn, firstName, lastName) {
 //     this.ssn = ssn;
