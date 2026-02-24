9c4649aa3756cd9b11f1f3cf16e8d93a4759136c
nshuti <nshutikope@gmail.com>
2026-02-24 08:47:22 +0200

    fix: Correct getSalary method signature and fix invocation in getFullname

--- AbstractClass.ts
+++ AbstractClass.ts
@@ -3,10 +3,10 @@ abstract class Person {
     constructor( private firstname: string, private lastname: string) {
         
     }
-    abstract getSalary() { }
+    abstract getSalary(): number { }
 
     getFullname() {
-        return `${this.firstname} ${this.lastname} and make ${this.getSalary}`;
+        return `${this.firstname} ${this.lastname} and make ${this.getSalary()}`;
     }
 }
 
@@ -18,5 +18,18 @@ class Emloyee extends Person {
         return this.Salary
     }
 }
+class EmployeFull extends Person {
+    constructor(firstname: string, lastname: string, private rate: number, private time: number) {
+        super(firstname, lastname);
+    }
+    getSalary(): number {
+        return this.rate * this.time
+    }
+}
 
+// const person = new EmployeFull("Nshuti", "maurice", 10, 60);
+let john = new Emloyee('John', 'Doe', 12000);
+let jane = new EmployeFull('Jane', 'Doe', 100, 160);
 
+console.log(john.getFullname());
+console.log(jane.getFullname());
