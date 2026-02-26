3539d78a697fe7f1baafdd570c7b7e01e80ae2b3
nshuti <nshutikope@gmail.com>
2026-02-26 16:56:57 +0200

    refactor: Clean up commented-out code and improve formatting in interfaceExtend.ts

--- interfaceExtend.ts
+++ interfaceExtend.ts
@@ -1,24 +1,62 @@
-/** @format */
+// /** @format */
+// interface Mailable {
+//   send(email: string): boolean;
+//   queue(email: string): boolean
+// }
+// interface FutureMailable extends Mailable {
+//     later( email: string, after: number) : boolean
+// }
+
+// // interface A {
+// //   a(): void;
+// // }
+
+// // interface B extends A {
+// //   b(): void;
+// // }
+
+
+
+// class Mail implements FutureMailable {
+//   later(email: string, after: number): boolean {
+//     console.log(`Send email to  ${email} in ${after}ms`);
+//     return true;
+//   }
+ 
+//   send(email: string): boolean {
+//     console.log(`Send email to  ${email} in ${after}ms`);
+
+//     return true;
+//   }
+
+//   queue(email: string): boolean {
+//     console.log(`Queue an email to ${email}`);
+
+//     return true;
+//   }
+// }
+
+
 interface Mailable {
   send(email: string): boolean;
-  queue(email: string): boolean
+  queue(email: string): boolean;
 }
+
 interface FutureMailable extends Mailable {
-    later( email: string, after: number) : boolean
+  later(email: string, after: number): boolean;
 }
 
-interface A {
-  a(): void;
+class Mail implements FutureMailable {
+  later(email: string, after: number): boolean {
+    console.log(`Send email to ${email} in ${after} ms.`);
+    return true;
+  }
+  send(email: string): boolean {
+    console.log(`Sent email to ${email} after ${after} ms. `);
+    return true;
+  }
+  queue(email: string): boolean {
+    console.log(`Queue an email to ${email}.`);
+    return true;
+  }
 }
-
-interface B extends A {
-  b(): void;
-}
-
-
-
-// class FutureMailable extends Mailable{
-//     send() {
-        
-//     }
-// }
