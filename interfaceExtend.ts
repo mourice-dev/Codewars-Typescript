46588f32f8ed8479b4a1ba9a15599c68a412fe36
nshuti <nshutikope@gmail.com>
2026-02-27 17:18:26 +0200

    refactor: Remove commented-out code and improve structure in interfaceExtend.ts

--- interfaceExtend.ts
+++ interfaceExtend.ts
@@ -15,14 +15,12 @@
 // //   b(): void;
 // // }
 
-
-
 // class Mail implements FutureMailable {
 //   later(email: string, after: number): boolean {
 //     console.log(`Send email to  ${email} in ${after}ms`);
 //     return true;
 //   }
- 
+
 //   send(email: string): boolean {
 //     console.log(`Send email to  ${email} in ${after}ms`);
 
@@ -36,27 +34,47 @@
 //   }
 // }
 
+// interface Mailable {
+//   send(email: string): boolean;
+//   queue(email: string): boolean;
+// }
 
-interface Mailable {
-  send(email: string): boolean;
-  queue(email: string): boolean;
+// interface FutureMailable extends Mailable {
+//   later(email: string, after: number): boolean;
+// }
+
+// class Mail implements FutureMailable {
+//   later(email: string, after: number): boolean {
+//     console.log(`Send email to ${email} in ${after} ms.`);
+//     return true;
+//   }
+//   send(email: string): boolean {
+//     console.log(`Sent email to ${email} after ${after} ms. `);
+//     return true;
+//   }
+//   queue(email: string): boolean {
+//     console.log(`Queue an email to ${email}.`);
+//     return true;
+//   }
+// }
+
+class Control {
+  private state: boolean;
 }
 
-interface FutureMailable extends Mailable {
-  later(email: string, after: number): boolean;
+interface StatefulControl extends Control {
+  enable(): void;
 }
 
-class Mail implements FutureMailable {
-  later(email: string, after: number): boolean {
-    console.log(`Send email to ${email} in ${after} ms.`);
-    return true;
-  }
-  send(email: string): boolean {
-    console.log(`Sent email to ${email} after ${after} ms. `);
-    return true;
-  }
-  queue(email: string): boolean {
-    console.log(`Queue an email to ${email}.`);
-    return true;
-  }
+class Button extends Control implements StatefulControl {
+  enable() {}
+}
+class TextBox extends Control implements StatefulControl {
+  enable() {}
+}
+class Label extends Control {}
+
+// Error: cannot implement
+class Chart implements StatefulControl {
+  enable() {}
 }
