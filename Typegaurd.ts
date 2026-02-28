86a8e239f7f41f810053d29d458a0552407a1bf6
nshuti <nshutikope@gmail.com>
2026-02-28 16:11:25 +0200

    feat: Update add function to return number or string for alphanumeric types

--- Typegaurd.ts
+++ Typegaurd.ts
@@ -4,10 +4,17 @@ type alphanumeric = string | number;
 // let a: alphanumeric = 1;
 // let b: alphanumeric = 2;
 
-function add(a: alphanumeric, b: alphanumeric): number {
+function add(a: alphanumeric, b: alphanumeric): number | string {
     if (typeof a === "number" && typeof b === "number") {
       return a + b;
     }
+
+    if(typeof a === 'string' && typeof b === 'string') {
+        return a.concat(b)
+    }
+    
+    throw new Error('Invalid arguments. Both arguments must be either numbers or strings.');
+
 }
 
-console.log(add(1,2))
+console.log(add(1, 2));
