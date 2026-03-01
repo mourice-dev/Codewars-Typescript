15d5fb1233b96cf21b197dcb404883d306a3777d
nshuti <nshutikope@gmail.com>
2026-03-01 12:00:00 +0200

    Backdated commit for March 1, 2026

---
+++ GreedisGood.ts
@@ -0,0 +1,21 @@
+// /** @format */
+// Greed is a dice game played with five six - sided dice.Your mission,
+//     should you choose to accept it, is to score a throw according to these rules.
+//     You will always be given an array with five six - sided dice values.
+
+//  Three 1's => 1000 points
+//  Three 6's =>  600 points
+//  Three 5's =>  500 points
+//  Three 4's =>  400 points
+//  Three 3's =>  300 points
+//  Three 2's =>  200 points
+//  One   1   =>  100 points
+//  One   5   =>   50 point
+// Each of 5 dice can only be counted once in each roll.For example, a given "5"
+//  can only count as part of a triplet(contributing to the 500 points) or as a single 50 points,
+//     but not both in the same roll.
+
+export function score(dice: number[]): number {
+  // Fill me in!
+  return 0;
+}
---
+++ TypeAssertion.ts
@@ -0,0 +1,5 @@
+/** @format */
+
+let el = document.querySelector('input["type="text"]');
+let a = el as HTMLInputElement;
+console.log(a.value);
