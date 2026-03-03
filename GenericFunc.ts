14a64470eb21e5f0cedafbc4a0f29b7020a0045e
nshuti <nshutikope@gmail.com>
2026-03-03 17:24:10 +0200

    refactor: Comment out unused returnRandomNumber function and related code

--- GenericFunc.ts
+++ GenericFunc.ts
@@ -1,15 +1,16 @@
-/** @format */
+// /** @format */
 
-function returnRandomNumber<T>(items: T[]): T {
-  let randoms = Math.floor(Math.random() * items.length);
-  return items[randoms];
-}
+// function returnRandomNumber<T>(items: T[]): T {
+//   let randoms = Math.floor(Math.random() * items.length);
+//   return items[randoms];
+// }
 
-let numbers = ["ns", "ma,", "ri"];
-let num = returnRandomNumber<string>(numbers);
-console.log(num);
+// let numbers = ["ns", "ma,", "ri"];
+// let num = returnRandomNumber<string>(numbers);
+// console.log(num);
 
-function merge<U, T>(obj1: U, obj2: T): U & T {
+
+function merge<U extends object, T extends object>(obj1: U, obj2: T): U & T {
     return {
         ...obj1, ...obj2
     }
@@ -18,3 +19,14 @@ function merge<U, T>(obj1: U, obj2: T): U & T {
 let result = merge({ name: "John" }, { jobTitle: "Frontend Developer" });
 
 console.log(result);
+
+
+function prop<U, T extends keyof U>(obj: U, k: T)  {
+    return obj[k];
+}
+
+// function prop<T, K extends keyof T>(obj: T, key: K) {
+//     return obj[key];
+// }
+let str = prop({ name: 'John' }, 'name');
+console.log(str);
