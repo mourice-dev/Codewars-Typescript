a5191c8579a9bff975319cdcb1160bf1cea396eb
nshuti <nshutikope@gmail.com>
2026-03-03 16:57:18 +0200

    fix: Format array elements and ensure consistent spacing in returnRandomNumber function

--- GenericFunc.ts
+++ GenericFunc.ts
@@ -1,11 +1,20 @@
 /** @format */
 
 function returnRandomNumber<T>(items: T[]): T {
-
   let randoms = Math.floor(Math.random() * items.length);
   return items[randoms];
 }
 
-let numbers = ["ns","ma,","ri"];
+let numbers = ["ns", "ma,", "ri"];
 let num = returnRandomNumber<string>(numbers);
 console.log(num);
+
+function merge<U, T>(obj1: U, obj2: T): U & T {
+    return {
+        ...obj1, ...obj2
+    }
+}
+
+let result = merge({ name: "John" }, { jobTitle: "Frontend Developer" });
+
+console.log(result);
