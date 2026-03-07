650c6d6def72b00720b1db92980850ce5b806e40
nshuti <nshutikope@gmail.com>
2026-03-07 17:46:43 +0200

    feat: Implement switchItUp function to convert numbers 0-9 to words

---
+++ switch it up.ts
@@ -0,0 +1,45 @@
+/** @format */
+// When provided with a number between 0-9, return it in words.
+// Note that the input is guaranteed to be within the range of 0-9.
+
+// Input: 1
+
+// Output: "One".
+
+// If your language supports it, try using a switch statement.
+
+export function switchItUp(intNumber: number): string {
+  switch (intNumber) {
+    case 1:
+      return "One";
+      break;
+    case 2:
+      return "Two";
+      break;
+    case 3:
+      return "Three";
+      break;
+    case 4:
+      return "Four";
+      break;
+    case 5:
+      return "Five";
+      break;
+    case 6:
+      return "Six";
+      break;
+    case 7:
+      return "Seven";
+      break;
+    case 8:
+      return "Eight";
+      break;
+    case 9:
+      return "Nine";
+      break;
+    default:
+      return "please enter number between 9-0";
+      break;
+  }
+}
+console.log(switchItUp(0));
