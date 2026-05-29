da432b25376d7760715cec51b70ceb26444a93e4
nshuti <nshutikope@gmail.com>
2026-05-29 14:34:29 +0200

    feat: Add Hex to Decimal solution

---
+++ Hex to Decimal.ts
@@ -0,0 +1,8 @@
+/** @format */
+
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+export function hexToDec(hexString: string): number {
+  return Number(hexString)
+}
+console.log(hexToDec("10"))
