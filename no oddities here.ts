f48c5eee466946856a14cda28745c13222660b53
nshuti <nshutikope@gmail.com>
2026-06-07 12:05:00 +0200

    feat: add no oddities here kata solution

---
+++ no oddities here.ts
@@ -0,0 +1,7 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+export function noOdds(values: number[]): number[] {
+  return values.filter((num) => num % 2 === 0);
+}
+console.log(noOdds([0, 1, 2, 3]));
